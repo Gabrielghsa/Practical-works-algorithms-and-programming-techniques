d9421382be0eb0e4859b3480275a681514079f7d
Gabriel Henrique Santos de Assis <ghassis2018@gmail.com>
2026-04-26 19:18:44 -0300

    Criação do repositório dos trabalhos práticos

---
+++ 1°-trabalho-prático/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        // Declaração de variáveis (duas strings e duas decimais (decimal é mais precisa para guardar valores monetários))
+        string formaPag, tipoClien;
+        decimal valCompra, descPorcen = 0, valfinal, valdesc;
+
+        //Entrada de dados
+        Console.WriteLine("Qual o valor total da compra?: ");
+        valCompra = decimal.Parse(Console.ReadLine());
+        Console.WriteLine("Qual a forma de pagamento?: D ou C");
+        formaPag = Console.ReadLine();
+        Console.WriteLine("Qual é o tipo de cliente?: N ou F");
+        tipoClien = Console.ReadLine();
+
+        //Pagamento em dinheiro
+        if (formaPag == "D")
+        {
+            if (valCompra <= 100)
+            {
+                descPorcen = 5;
+            }
+            else
+            {
+                descPorcen = 10;
+            }
+        }
+
+        //Pagamento em cartão
+        if (formaPag == "C")
+        {
+            if (valCompra <= 100)
+            {
+                descPorcen = 0;
+            }
+            else if (valCompra <= 300)
+            {
+                descPorcen = 5;
+            }
+            else
+            {
+                descPorcen = 10;
+            }
+        }
+
+        //Benefício adicional pelo tipo de cliente 
+        if (tipoClien == "F")
+        {
+            if (valCompra > 200)
+            {
+                descPorcen += 5;
+            }
+            if (formaPag == "D")
+            {
+                descPorcen += 2;
+            }
+        }
+        //limitando o valor da porcentagem do desconto
+        if (descPorcen > 15)
+        {
+            descPorcen = 15;
+        }
+
+        //calculos do valor do desconto e do valor final a ser pago 
+        valdesc = valCompra * descPorcen / 100;
+        valfinal = valCompra - valdesc;
+
+        //saída de dados 
+        Console.WriteLine("--------------------------------------");
+        Console.WriteLine($"O valor original da compra é: R${valCompra:f2}");
+        Console.WriteLine($"O percentual de desconto é: {descPorcen}%");
+        Console.WriteLine($"O valor do desconto é: R${valdesc:f2}");
+        Console.WriteLine($"O valor final para pagar é: R${valfinal:f2}");
+        Console.ReadKey();
+    }
+}
---
+++ 2°-trabalho-pratico/Program.cs
@@ -0,0 +1,68 @@
+﻿int n, voto, numCand1, numCand2, votoCand1, votoCand2, nulo, branco;
+do
+{
+    //Zerando as váriáveis dentro do loop para evitar problemas de acumulação
+    votoCand1 = 0;
+    votoCand2 = 0;
+    nulo = 0;
+    branco = 0;
+    Console.WriteLine("Digite a quantidade de eleitores: ");
+    n = int.Parse(Console.ReadLine());
+
+    while (n < 10)
+    {
+        Console.WriteLine("Quantidade de eleitores inválida.");
+        Console.WriteLine("Digite a quantidade de eleitores: ");
+        n = int.Parse(Console.ReadLine());
+    }
+    Console.WriteLine($"Digite o número do candidato Teobaldo");
+    numCand1 = int.Parse(Console.ReadLine());
+    Console.WriteLine($"Digite o número do candidato Astrogildo");
+    numCand2 = int.Parse(Console.ReadLine());
+
+    //Evitando números iguais para os candidatos
+    while (numCand1 == numCand2)
+    {
+        Console.WriteLine("Números iguais. Digite novamente o número do segundo candidato.");
+        numCand2 = int.Parse(Console.ReadLine());
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        Console.Write("Digite o número do seu voto: ");
+        voto = int.Parse(Console.ReadLine());
+        if (voto == numCand1)
+        {
+            votoCand1++;
+        }
+        else if (voto == numCand2)
+        {
+            votoCand2++;
+        }
+        else if (voto == 0)
+        {
+            branco++;
+        }
+        else
+        {
+            nulo++;
+        }
+    }
+    if (votoCand1 > votoCand2)
+    {
+        Console.WriteLine($"Vitória do canditado Teobaldo com: {votoCand1} votos.");
+    }
+    else if (votoCand2 > votoCand1)
+    {
+        Console.WriteLine($"Vitória do canditado Astrogildo com: {votoCand2} votos.");
+    }
+    else
+    {
+        Console.WriteLine("Empate! Nova eleição deve ser realizada.");
+    }
+    Console.WriteLine($"O candidato Teobaldo obteve: {votoCand1} votos.");
+    Console.WriteLine($"O candidato Astrogildo obteve: {votoCand2} votos.");
+    Console.WriteLine($"A quantidade de votos nulos foi: {nulo}.");
+    Console.WriteLine($"A quantidade de votos em branco foi: {branco}.");
+} while (votoCand1 == votoCand2);
+
